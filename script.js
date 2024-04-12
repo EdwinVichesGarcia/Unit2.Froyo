0d87799ebe69f23666839f292fc5b251f8bf6759
EdwinVichesGarcia <edwinviches@gmail.com>
2024-04-11 17:31:21 -0700

    Submission

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log("Froyo Orders");
+// Using a prompt to have user enter the froyo flavors
+const froyoFlavors = prompt(
+    "Type you Froyo flavor sepertated by commas.");
+
+
+const froyoArray = froyoFlavors.split(',');
+
+
+function froyoCount(flavors) {
+    const count = {};
+    froyoArray.foreach(flavors =>){
+      if (froyoCount ++){
+
+      }else (froyoCount =1);
+    }
+}
+return count;
+
+
+const count = froyoCount(froyoArray);
